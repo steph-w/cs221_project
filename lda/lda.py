70aab017fd0e179e408ad185ddea9ce6c22f9a59
Stephanie Wang <steph17@stanford.edu>
2016-11-16 21:23:34 +0000

    added LDA implementation

--- lda/lda.py
+++ lda/lda.py
@@ -1,6 +1,7 @@
 # Latent Dirichlet Allocation Implementation, using Gibbs Sampling
-import os
-from random import randint
+import os, collections
+import random
+from pdb import set_trace as t
 
 ROOT = '../'
 DATA_DIRECTORY = os.path.join(ROOT, "data/journal_ai_research_abstracts/cleaned/")
@@ -20,6 +21,29 @@ def read_data(data_directory):
         break
     return data
 
+# Function: Weighted Random Choice
+# --------------------------------
+# Given a dictionary of the form element -> weight, selects an element
+# randomly based on distribution proportional to the weights. Weights can sum
+# up to be more than 1. 
+def weightedRandomChoice(weightDict):
+    weights = []
+    elems = []
+    for elem in weightDict:
+        weights.append(weightDict[elem])
+        elems.append(elem)
+    total = sum(weights)
+    key = random.uniform(0, total)
+    runningTotal = 0.0
+    chosenIndex = None
+    for i in range(len(weights)):
+        weight = weights[i]
+        runningTotal += weight
+        if runningTotal > key:
+            chosenIndex = i
+            return elems[chosenIndex]
+    raise Exception('Should not reach here')
+
 class LDA:
     def __init__(self, data):
         """
@@ -30,6 +54,7 @@ class LDA:
         self.data = data
         self.num_iterations = 1
         self.corpus = [] # list of all words, length N
+        self.doc_pointers = [] # list of document each word belongs to, length N
         self.num_topics = 5 # K
         # TODO don't know what these superparameters should be
         self.alpha = 0.5
@@ -40,11 +65,11 @@ class LDA:
         creates the corpus from given data
         """
         print "Generating corpus."
-        corpus = set()
-        for _, text in self.data.iteritems():
+        for doc_id, doc in enumerate(self.data):
+            text = self.data[doc]
             for word in text.split():
-                corpus.add(word)
-        self.corpus = list(corpus)
+                self.corpus.append(word)
+                self.doc_pointers.append(doc_id)
 
     def run(self):
         """
@@ -52,27 +77,37 @@ class LDA:
         """
         print "Performing LDA. (not implemented)"
         # TODO: are all below these initialized correctly?
-        assignments = [randint(0,self.num_topics) for i in range(len(self.corpus))] # z
-        # n_dk[d][k] = number of words from doc d assigned to topic k
-        # n_kw[k][w] = number of times word w is assigned to topic k
+        assignments = [random.randint(0,self.num_topics-1) for i in range(len(self.corpus))] # z
+        # n_dk[k][d] = number of words from doc d assigned to topic k
+        # n_kw[w][k] = number of times word w is assigned to topic k
         # n_k = number of times any word assigned to topic k
         n_dk = [[1 for i in range(len(data))] for j in range(self.num_topics)]
         n_kw = [[1 for i in range(self.num_topics)] for j in range(len(self.corpus))]
         n_k =  [1 for i in range(self.num_topics)]
-
         for ii in range(self.num_iterations):
             for jj in range(len(self.corpus)):
-                pass
-
-
-
-
-
+                weights = collections.defaultdict(float)
+                word = self.corpus[jj]
+                doc = self.doc_pointers[jj]
+                topic = assignments[jj]
+                n_dk[topic][doc] -= 1
+                n_kw[jj][topic] -=1
+                n_k[topic] -= 1
+                for kk in range(self.num_topics):
+                    # TODO: Check this weight assignment
+                    weights[topic] = (n_dk[kk][doc]+self.alpha)*((n_kw[jj][kk]+self.beta)/(n_k[kk]+self.beta))
+                topic = weightedRandomChoice(weights)
+                assignments[jj] = topic
+                n_dk[topic][doc] +=1
+                n_kw[jj][topic] += 1
+                n_k[topic] += 1
+        return assignments, n_dk, n_kw, n_k
 if __name__ == "__main__":
     print
     data = read_data(DATA_DIRECTORY)
     lda = LDA(data)
     lda.generate_corpus()
+    # lda.generate_document_corpus()
     topics = lda.run()
     print "done."
     print
