b719ade944a20b23b5a0ea4ff7d109ddc176d32a
Stephanie Wang <steph17@stanford.edu>
2016-12-13 18:56:58 +0000

    added articles_no_stopwords

--- utils/data_processing/remove_stop_words.py
+++ utils/data_processing/remove_stop_words.py
@@ -1,9 +1,10 @@
 import os
 from nltk.corpus import stopwords
 
-IF = './clean'
-OF = './clean_no_stopwords'
-CUSTOM_WORDS = ('the', 'if', 'this', 'but', 'in', 'at', 'that', 'by', 'these', 'we', 'as')
+IF = './split_by_article'
+OF = './articles_no_stopwords'
+CUSTOM_WORDS = ('the', 'if', 'this', 'but', 'in', 'at', 'that', 
+	'by', 'these', 'we', 'as', 'also', 'like', 'fi', 'ff')
 cachedStopWords = set(stopwords.words("english"))
 cachedStopWords.update(CUSTOM_WORDS)
 
@@ -12,13 +13,14 @@ if not os.path.exists(OF):
 
 for r,ds,fs in os.walk(IF):
 	for f in fs:
-
 		text = []
 		with open(os.path.join(r,f), 'r') as fr:
 			text = fr.readlines()
 		print "removing stop words: " + f
 		goodtext = [' '.join(filter(lambda x: x.lower() not in cachedStopWords, line.split())) + '\n' for line in text]
-		of_name = os.path.join(OF, f)
-		with open(of_name, 'w') as fw:
+		of_path_name = os.path.join(OF,os.path.basename(r))
+		if not os.path.exists(of_path_name):
+			os.mkdir(of_path_name)
+		of_name = os.path.join(of_path_name, f)
+		with open(of_name,'w') as fw:
 			fw.writelines(goodtext)
-
